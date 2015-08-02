35bd75934d703cf7afb83aea88aeef478dc83871
andrew-shaw <shawa1@tcd.ie>
2015-08-02 11:49:12 +0100

    Clean up, clear up comments a tad.

--- project/bot.py
+++ project/bot.py
@@ -2,29 +2,28 @@
 from itertools import islice, chain
 from collections import Counter
 import numpy as np
-import random
-import json
 import networkx as nx
 
 # ASCII Start/End text. It's the best I can come up with
 # for a sentinel 'start of sentence' marker that won't show up
-# in training text (´・ω・`)
+# in training text ¯\_(ツ)_/¯
 STX = '\x02'
 ETX = '\x03'
 
 
 def normalize(xs):
+    '''Normalize a list of values to fit into a probability distribution'''
     s = sum(xs)
     return [i/s for i in xs]
 
 
 def generate_next_word_graph(text):
-    '''Build the dictionary of {word: [next word]}
+    '''Build a graph representing a Markov chain from a training corpus
     text: Training text, must be an iterable of 'statements', where a sentence
           is a meaningful grouping of words, e.g. a sentence, tweet,
-          source code statement etc.
+          source code line etc.
     '''
-    # This essentially models a weighted directed graph, where each vertex v_i
+    # This is weighted directed graph, where each vertex v_i
     # is a word, and each edge (v_i, v_j) models v_j appearing in the text
     # after v_i.  Edge weights w_ij denote frequency, that is w_ij > w_ik
     # implies v_j appears more frequently after v_k
@@ -33,19 +32,16 @@ def generate_next_word_graph(text):
             words = line.split()
             yield (zip(([STX] + words), words + [ETX]))
     counted_pairs = Counter(chain(*word_pairs())).items()
-    weigthed_edges = ((u, v, w) for ((u, v), w) in counted_pairs)
+    weighted_edges = ((u, v, w) for ((u, v), w) in counted_pairs)
     counts = nx.DiGraph()
-    counts.add_weighted_edges_from(weigthed_edges)
+    counts.add_weighted_edges_from(weighted_edges)
     return counts
-
-# TODO: counter
-
-
-def serialze_next_word_graph(graph):
-    print(json.dumps(graph))
 
 
 def sentence(word_graph):
+    '''Generate a 'sentence' from the training data
+    word_graph: directed graph of words & weights
+    '''
     current = STX
     while True:
         neighbours = word_graph[current]
@@ -66,14 +62,11 @@ def main(*, training_file='texts/tweets.txt', n=10, s=False):
     '''
     with open(training_file, 'r') as f:
         words = generate_next_word_graph(f)
-    if s:
-        serialze_next_word_graph(words)
-    else:
-        for _ in range(n):
-            for word in sentence(words):
-                print(word, end=' ')
-            print()
 
+    for _ in range(n):
+        for word in sentence(words):
+            print(word, end=' ')
+        print()
 
 
 if __name__ == '__main__':
